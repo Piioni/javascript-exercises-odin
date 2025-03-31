97e5def75da793f163b48584f54fffa77ee3e454
Piioni <rangelc.juan@gmail.com>
2025-04-01 00:52:23 +0200

    Fourth exercise passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // Create a new array to store the filtered values
+    const filteredArray = [];
+    // Loop through each element in the original array
+    for (let i = 0; i < array.length; i++) {
+        // Check if the current element is not in the args
+        if (!args.includes(array[i])) {
+            // If not, add it to the filtered array
+            filteredArray.push(array[i]);
+        }
+    }
+    // Return the filtered array
+    return filteredArray;
 };
 
 // Do not edit below this line
