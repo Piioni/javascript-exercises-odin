b306df06b1ef4111055152bf9dca4d576aa6e49c
Piioni <rangelc.juan@gmail.com>
2025-04-01 00:57:34 +0200

    Sixth exercise passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Check if the year is divisible by 4
+    if (year % 4 === 0) {
+        // Check if the year is divisible by 100
+        if (year % 100 === 0) {
+        // Check if the year is divisible by 400
+        return year % 400 === 0;
+        }
+        return true; // Year is a leap year
+    }
+    return false; // Year is not a leap year
 
 };
 
